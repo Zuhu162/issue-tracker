c93c48cc4f487c55fa69d9c02f2ec2d9773e4e99
Zuhu162 <zuhayersiddique162@gmail.com>
2024-03-16 16:16:27 +0800

    Prisma Changes

--- prisma/migrations/20240220035316_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updatedAt` on the `issue` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `issue` DROP COLUMN `updatedAt`;
